585de66eb8e56ce3e931e690776736451d845aee
Christian Lee Seibold <christian.seibold32@outlook.com>
2025-02-11 20:00:37 -0600

    Web Server: Make auragem_http the default for all hostnames not otherwise specified

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -92,7 +92,7 @@ func startTorWebServer(t *tor.Tor) {
 
 func startWebServer() {
 	httpMuxer := http.NewServeMux()
-	httpMuxer.Handle("auragem.ddns.net/", http.FileServer(http.Dir("/home/clseibold/ServerData/auragem_sis/SIS/auragem_http")))
+	httpMuxer.Handle("/", http.FileServer(http.Dir("/home/clseibold/ServerData/auragem_sis/SIS/auragem_http")))
 	httpMuxer.Handle("scrollprotocol.us.to/", http.FileServer(http.Dir("/home/clseibold/ServerData/auragem_sis/SIS/scrollprotocol_http")))
 	httpMuxer.Handle("auragemhkzsr5rowsaxauti6yhinsaa43wjtcqxhh7fw5tijdoqbreyd.onion/", http.FileServer(http.Dir("/home/clseibold/ServerData/auragem_sis/SIS/auragem_tor_http")))
 	err := http.ListenAndServe("0.0.0.0:80", httpMuxer)
