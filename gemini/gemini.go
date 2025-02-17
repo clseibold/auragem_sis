98c54960cb087c8071a92af96419f89818042cbd
Christian Lee Seibold <christian.seibold32@outlook.com>
2025-02-17 06:51:38 -0600

    Add test cgi route to gemini server

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -164,6 +164,7 @@ func setupAuraGem(context *sis.SISContext, chatContext *chat.ChatContext) {
 	geminiServer.AddFile("/.well-known/security.txt", "./security.txt")
 	geminiServer.AddProxyRoute("/nex/*", "$auragem_nex/*", '1')
 	geminiServer.AddUploadRoute("/guestbook.gmi", handleGuestbook)
+	geminiServer.AddCGIRoute("/debug", "/home/clseibold/ServerData/cgi_test")
 
 	// Proxies
 	youtube.HandleYoutube(geminiServer)
